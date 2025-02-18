e718ba01cde2f3fa18a9c8f8aafc1290fcbfe0d3
Andre Damasceno <andrelbd1@gmail.com>
2025-02-18 00:16:07 -0300

    Adding documentation to process report task

--- src/tasks/process_report.py
+++ src/tasks/process_report.py
@@ -12,7 +12,26 @@ config_app = ApplicationConfig()
 
 
 @celery_seedz.task(bind=True, queue=config_app.QUEUE_PROCESS_REPORT)
-def process_report(self, **params):
+def process_report(self, **params: dict):
+    """
+    Processes a report asynchronously.
+
+    This Celery task processes a report based on the provided parameters.
+    It logs the progress and handles any exceptions that occur during the process.
+
+    Args:
+        self (Task): The Celery task instance.
+        **params (dict): A dictionary of parameters required for processing the report. Expected keys include:
+            - individual_id (str): The ID of the individual.
+            - unique_id (str): A unique identifier for the report.
+
+    Raises:
+        Exception: If an error occurs during the report processing, it is logged, and the error is set in the report.
+
+    Logs:
+        Logs the completion of the report or any exceptions that occur,
+        along with the provided individual_id and unique_id.
+    """
     try:
         extra = {
             "individual_id": params.get("individual_id"),
@@ -31,5 +50,13 @@ def process_report(self, **params):
         controller.db_disconnect()
 
 
-def queue_process_report(params):
+def queue_process_report(params: dict):
+    """
+    Queue the process_report task with the given parameters.
+
+    This function uses Celery to asynchronously queue the process_report task.
+
+    Args:
+        params (dict): A dictionary of parameters to pass to the process_report task.
+    """
     process_report.delay(**params)

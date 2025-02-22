828494d1797f14050ed2439b63d78ce4c8f5541f
Andre Damasceno <andrelbd1@gmail.com>
2025-02-21 22:43:39 -0300

    Change criterias

--- migrations/versions/e82ac4f587a0_adding_criterias.py
+++ migrations/versions/e82ac4f587a0_adding_criterias.py
@@ -52,15 +52,43 @@ def memory_consume():
         ]
     }
 
-def setup_time():
+def count_nodes():
     return {
         'id': '92771dd3-811d-43d3-ad9a-adc2a2c672db',
-        'name': 'Setup Time',
-        'desc': 'Measures the time taken to initialize any data structures or prerequisites before running the algorithm.',
+        'name': 'Count nodes',
+        'desc': 'Count the number of nodes in the graph.',
         'dt': "2024-10-18 21:08:00",
         'algorithms': [
             {
-                'id': '1e740c1b-1ea6-4c0f-bcba-fb939890bc60',
+                'id': '499b32f9-e2fc-4eb8-860e-fafdb8145b7f',
+                'alg_id': '0192919b-2501-91c1-d4bb-c71b4c0785d5',  # dijkstra
+            }
+        ]
+    }
+
+def count_edges():
+    return {
+        'id': '9f59f0dd-2e0e-4c40-99ad-5f0ba5ac2e32',
+        'name': 'Count edges',
+        'desc': 'Count the number of edges in the graph.',
+        'dt': "2024-10-18 21:08:00",
+        'algorithms': [
+            {
+                'id': 'afacccde-a1d6-4acd-a152-1428c47027fa',
+                'alg_id': '0192919b-2501-91c1-d4bb-c71b4c0785d5',  # dijkstra
+            }
+        ]
+    }
+
+def detect_cycle():
+    return {
+        'id': '3008715f-0d3f-445e-9d58-3bd57ccb681d',
+        'name': 'Detect cycle',
+        'desc': 'Identify if there are any cycles within the graph structure.',
+        'dt': "2024-10-18 21:08:00",
+        'algorithms': [
+            {
+                'id': '209262a2-1662-4d30-9136-57320f117c9f',
                 'alg_id': '0192919b-2501-91c1-d4bb-c71b4c0785d5',  # dijkstra
             }
         ]
@@ -70,7 +98,9 @@ def upgrade():
     values = []
     values.append(running_time())
     values.append(memory_consume())
-    values.append(setup_time())
+    values.append(count_nodes())
+    values.append(count_edges())
+    values.append(detect_cycle())
     cols_c = f"""criteria_id, name, description, created_at, updated_at, enabled"""
     cols_a = f"""algorithm_criteria_id, algorithm_id, criteria_id, created_at, updated_at, enabled"""
     for v in values:
@@ -85,7 +115,9 @@ def downgrade():
     values = []
     values.append(running_time())
     values.append(memory_consume())
-    values.append(setup_time())
+    values.append(count_nodes())
+    values.append(count_edges())
+    values.append(detect_cycle())
     c_ids = [v['id'] for v in values]
     c_ids = tuple(c_ids)
     report_id = f"""SELECT report_id FROM service_algorithm_analysis.result WHERE criteria_id in {c_ids}"""

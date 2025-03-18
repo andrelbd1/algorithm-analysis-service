b153d7ad5b4c8c03a4c2012df7dff475ce2ee14f
André Brandão <andrelbd1@gmail.com>
2025-03-17 17:03:53 -0700

    Add report handling endpoints and Swagger models for report creation

--- src/routes.py
+++ src/routes.py
@@ -2,6 +2,7 @@ from tornado.web import url
 
 from src.api.healthcheck import AlgorithmAnalysisService
 from src.api.v1.algorithm import ViewDeleteAlgorithm, ViewGetAlgorithm
+from src.api.v1.report import ViewGetReport, ViewPostReport
 
 
 class Routes:
@@ -9,4 +10,5 @@ class Routes:
         url(r'/healthcheck', AlgorithmAnalysisService),
         url(r'/v1/algorithm/([^/]*)', ViewDeleteAlgorithm),
         url(r'/v1/algorithm', ViewGetAlgorithm),
+        url(r'/v1/report', ViewPostReport),
     ]
